6f58db653fb07dc52aed207173012f8999d746e4
serazs <discord2016qwe@gmail.com>
2026-05-09 20:58:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function createBot() {
         host: 'suriboom.aternos.me', 
         port: 21431,                  
         username: '.DJ_Semazen',       
-        version: '1.21.1'
+        version: '1.21'
     });
 
     let musicInterval;
